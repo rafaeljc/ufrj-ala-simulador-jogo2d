2eb6a271cb6dcb2330bc3f8dd8d1bf8dbc36a05c
rafael <rafaeljclemente@dcc.ufrj.br>
2019-10-18 09:46:35 -0300

    Adicionada função para desenhar na tela

--- Jogo2D.py
+++ Jogo2D.py
@@ -1,3 +1,9 @@
+'''
+
+ALA Simulador Jogo2D
+https://github.com/rafaeljc/ala-simulador-jogo2d
+
+'''
 
 # instalação:   python3 -m pip install --user numpy
 import numpy as np
@@ -6,22 +12,77 @@ import numpy as np
 import pygame, sys
 from pygame.locals import *
 
-from constantes import *
-from objetos import *
+
+
+'''
+CONSTANTES
+'''
+# cores (em RGB)
+VERMELHO = (255, 0, 0)
+PRETO = (0, 0, 0)
+
+# configurações
+TELA_TITULO = 'ALA 2019.2 - Simulador Jogo2D'
+TELA_LARGURA = 800
+TELA_ALTURA = 600
+FPS = 15
+COR_FUNDO = PRETO
+
+
+
+'''
+NAVE
+'''
+class Nave:
+
+    def __init__(self):
+        # vértices que formam a nave
+        self.vertices = np.array([[385., 400., 415., 400., 385.],
+                                  [310., 280., 310., 300., 310.],
+                                  [1.,   1.,   1.,   1.,   1.  ]])
+        # centro da nave
+        self.centro = np.array([[400.], 
+                                [300.],
+                                [1.  ]])
+        # vetor que orienta a movimentação da nave
+        # módulo vetor: velociade da nave
+        self.orientacao = np.array([[0.],
+                                    [1.],
+                                    [1.]])
+        # largura traço da nave
+        self.traco_largura = 3
+        # cor traço da nave
+        self.traco_cor = VERMELHO
+        # desenha a nave na tela ligando os vértices
+
+    def desenha(self):
+        quantidade_colunas = self.vertices.shape[1]
+        for i in range(quantidade_colunas - 1):
+            ponto_inicial = (self.vertices[0,i], self.vertices[1,i])
+            ponto_final = (self.vertices[0,i+1], self.vertices[1,i+1])
+            pygame.draw.line(DISPLAYSURF, self.traco_cor, ponto_inicial, ponto_final, self.traco_largura)
+        return
 
 
 
 def main():
 
-    global FPSCLOCK, DISPLAYSURF
+    iniciar()
+
+    while True:
+        jogar()
+
+
+
+def iniciar():
+
+    global FPSCLOCK, DISPLAYSURF, nave
 
     pygame.init()
     FPSCLOCK = pygame.time.Clock()
     DISPLAYSURF = pygame.display.set_mode((TELA_LARGURA, TELA_ALTURA))
     pygame.display.set_caption(TELA_TITULO)
-
-    while True:
-        jogar()
+    nave = Nave()   # instancia a nave
 
 
 
@@ -37,11 +98,9 @@ def jogar():
                 if event.key == K_x:
                     sair()
 
-        v = np.array([[10.0, 50.0], 
-                      [10.0, 50.0]])
-
+        # atualiza tela
         DISPLAYSURF.fill(COR_FUNDO)
-        pygame.draw.line(DISPLAYSURF, VERMELHO, v[:,0], v[:,1], 5)
+        nave.desenha()
         pygame.display.update()
         FPSCLOCK.tick(FPS)
 
